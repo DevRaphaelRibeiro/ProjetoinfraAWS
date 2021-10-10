120c4b8a8a40cdacfb64976499f28acdf0aa6587
Raphael Ribeiro <rafael.r1@hotmail.com>
2021-10-10 10:27:30 -0300

    Alterando a main

--- main.tf
+++ main.tf
@@ -12,10 +12,6 @@ provider "aws" {
   region = var.region
 
 }
-/*resource "aws_key_pair" "key-ks8" {
-    key_name = "key-ks8"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDSTGANtfmtE7rYtOoekCrhZ2S6lrRzFAiT5uFu58cVc88/zD65i0UiycFG/Z3TAt7Fp/39dTN64nCsC66LETjgC4uZKYSg8RslmMoHCf+oLLla5NsidffVfchl2oyh0wh5nq9xVtftfYF81wyUKG21rA3nKSFT8fyfBJJJDhOl2Ebw7ljQmRack8WJWArPYJ2F5mxFeiUb7hCb3C7Dh7kzrKxtIiCrFJ0U5dQsJkXOr5gE/HYY2FDuW7x/rB+YSgEmKnDfmr+fXolQHwCWz3pwe2PSsSxgqE+IhFcI7mV0mg877y57WkkeafpJWVMGQ/cD7iIVB8LbFfacKp3DV7ICzTS/nIPddMaPlEzEMJCvKvFbouPMCkRIkLFvJWjEggeUeA8loX3oc3M69yD4uSfeR4aZvxW7CbZrTQnxSdWVwqeH+cRu3+JvG6Ne3L+3ksiZ0XqV2aEp8wmmn92+FIDMKl3bVd48OL1xxDMWAltE1sQuPwF8t/Y11mAZMAOwS1M= raphael@DESKTOP-QT3IKI6"   
-}*/
 
 resource "aws_security_group" "k8s-sg" {
     
